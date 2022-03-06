93cb645d2628f8d5d57c5a537da4ed72d05fd231
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-03-06 19:52:47 +0100

    update tutorial

--- ReceiptSplitter/View/Tutorials/ScanTutorialView.swift
+++ ReceiptSplitter/View/Tutorials/ScanTutorialView.swift
@@ -8,73 +8,178 @@
 import SwiftUI
 
 struct ScanTutorialView: View {
+    @EnvironmentObject var model: ModelData
+    
     var body: some View {
+        
         VStack {
-            Image("receipt_tutorial")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .padding()
-                .frame(maxHeight: 300)
             
-            VStack(alignment: .leading) {
-                HStack(alignment: .center) {
-                    Image(systemName: "viewfinder")
-                        .frame(width: 30, height: 30)
-                        .font(.largeTitle)
-                        .foregroundColor(.yellow)
+            if !model.parameters.bigRecognition {
+                VStack {
+                    Image("receipt_tutorial")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
                         .padding()
-
-                    VStack(alignment: .leading, spacing: 2) {
-                        Text("Center your scan ")
-                            .font(.headline)
-                            .foregroundColor(.primary)
-
-                        Text("Scan the items and their prices, not what is above or below on the receipt")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .fixedSize(horizontal: false, vertical: true)
-                    }
+                        .frame(maxHeight: 300)
                     
-                }
-                
-                HStack(alignment: .center) {
-                    Image(systemName: "skew")
-                        .frame(width: 30, height: 30)
-                        .font(.largeTitle)
-                        .foregroundColor(.yellow)
-                        .padding()
+                    Button {
+                        withAnimation() {
+                            model.parameters.bigRecognition = true
+                        }
+                    } label: {
+                        HStack {
+                            Image(systemName: "wand.and.stars")
+                                .font(.title2)
+                                .padding(2)
+                            VStack(alignment: .leading) {
+                                Text("**Advanced Recognition** is **off**")
+                                Text("Tap to enable once.\nChange the default in settings.")
+                                    .font(.caption2)
+                            }
+                            .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .buttonStyle(.bordered)
+                    .tint(.teal)
+                    .padding(.bottom, 7)
+                    
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .center) {
+                            Image(systemName: "viewfinder")
+                                .frame(width: 30, height: 30)
+                                .font(.largeTitle)
+                                .foregroundColor(.yellow)
+                                .padding()
 
-                    VStack(alignment: .leading, spacing: 2) {
-                        Text("Adjust if necessary")
-                            .font(.headline)
-                            .foregroundColor(.primary)
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Center your scan ")
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
 
-                        Text("You can manually adjust the frame such that only the right content appears")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .fixedSize(horizontal: false, vertical: true)
+                                Text("Scan the items and their prices, not what is above or below on the receipt")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                            
+                        }
+                        
+                        HStack(alignment: .center) {
+                            Image(systemName: "skew")
+                                .frame(width: 30, height: 30)
+                                .font(.largeTitle)
+                                .foregroundColor(.yellow)
+                                .padding()
+
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Adjust if necessary")
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
+
+                                Text("You can manually adjust the frame such that only the right content appears")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                        }
+                        
+                        HStack(alignment: .center) {
+                            Image(systemName: "goforward.plus")
+                                .frame(width: 30, height: 30)
+                                .font(.largeTitle)
+                                .foregroundColor(.yellow)
+                                .padding()
+
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Add several scans")
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
+
+                                Text("If you have a long receipt, you can add multiple scans, but make sure not to scan the same item twice")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                        }
                     }
                 }
-                
-                HStack(alignment: .center) {
-                    Image(systemName: "goforward.plus")
-                        .frame(width: 30, height: 30)
-                        .font(.largeTitle)
-                        .foregroundColor(.yellow)
+                .transition(.move(edge: .bottom))
+
+            } else {
+                VStack {
+                    Image("receipt_tutorial_AR")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
                         .padding()
+                        .frame(maxHeight: 300)
+                    
+                    Button {
+                        withAnimation() {
+                            model.parameters.bigRecognition = false
+                        }
+                    } label: {
+                        HStack {
+                            Image(systemName: "wand.and.stars")
+                                .font(.title2)
+                                .padding(2)
+                            VStack(alignment: .leading) {
+                                Text("**Advanced Recognition** is **on**!")
+                                Text("Tap to disable once.\nChange the default in settings.")
+                                    .font(.caption2)
+                            }
+                            .multilineTextAlignment(.leading)
+                        }
+                    }
+                    .buttonStyle(.borderedProminent)
+                    .tint(.teal)
+                    .padding(.bottom, 7)
+                    
+                    VStack(alignment: .leading) {
+                        HStack(alignment: .center) {
+                            Image(systemName: "viewfinder")
+                                .frame(width: 30, height: 30)
+                                .font(.largeTitle)
+                                .foregroundColor(.yellow)
+                                .padding()
 
-                    VStack(alignment: .leading, spacing: 2) {
-                        Text("Add several scans")
-                            .font(.headline)
-                            .foregroundColor(.primary)
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Scan the whole receipt")
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
 
-                        Text("If you have a long receipt, you can add multiple scans, but make sure not to scan the same item twice")
-                            .font(.subheadline)
-                            .foregroundColor(.secondary)
-                            .fixedSize(horizontal: false, vertical: true)
+                                Text("Advanced Recognition automatically identifies your items, and compares them to the total price")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                            
+                        }
+
+                        
+                        HStack(alignment: .center) {
+                            Image(systemName: "rectangle.split.2x1.slash")
+                                .frame(width: 30, height: 30)
+                                .rotationEffect(.degrees(90.0))
+                                .font(.largeTitle)
+                                .foregroundColor(.yellow)
+                                .padding()
+
+                            VStack(alignment: .leading, spacing: 2) {
+                                Text("Do not split")
+                                    .font(.headline)
+                                    .foregroundColor(.primary)
+
+                                Text("Make only one scan per receipt")
+                                    .font(.subheadline)
+                                    .foregroundColor(.secondary)
+                                    .fixedSize(horizontal: false, vertical: true)
+                            }
+                        }
                     }
                 }
+                .transition(.move(edge: .bottom))
             }
+
         }
         .frame(maxWidth: 300)
         
