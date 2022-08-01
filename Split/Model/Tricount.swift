e049ffeea9583115bc8c45fe5224f64f5feac20a
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-08-01 15:18:01 +0200

    remove need for WebView dependency

--- Split/Model/Tricount.swift
+++ Split/Model/Tricount.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import WebView
+import WebKit
 
 
 struct Tricount: Codable { //default values
@@ -16,26 +16,28 @@ struct Tricount: Codable { //default values
 }
 
 
-func getInfoFromTricount(tricountID: String) async throws -> Tricount{
-    let webViewStore = WebViewStore()
+func getInfoFromTricount(tricountID: String) async throws -> Tricount {
+    let webView = WKWebView()
+    
     var tricount = Tricount()
     tricount.tricountID = tricountID
     
     let tricountLink = URL(string: "https://api.tricount.com/displayTricount.jsp?tricountID=\(tricountID)&acceptGACookies=true")!
     
-    await webViewStore.webView.load(URLRequest(url: tricountLink))
+    await webView.load(URLRequest(url: tricountLink))
     
     try await Task.sleep(nanoseconds: UInt64(2 * Double(NSEC_PER_SEC)))
     
-    // Conitnuations help from:
+    // Continuations help from:
     //  https://www.hackingwithswift.com/quick-start/concurrency/how-to-use-continuations-to-convert-completion-handlers-into-async-functions
     //  https://stackoverflow.com/questions/70329835/call-to-main-actor-isolated-instance-method-xxx-in-a-synchronous-nonisolated-con
+    //  https://stackoverflow.com/questions/39793459/xcode-8-ios-10-starting-webfilter-logging-for-process (remove warnings)
     
     let getTricountName = #"document.querySelector('div[class="tricountTitle"]').textContent"#
     let tricountName: String = await withCheckedContinuation { continuation in
 
         Task{
-            await webViewStore.webView.evaluateJavaScript(getTricountName) { res, error in
+            await webView.evaluateJavaScript(getTricountName) { res, error in
                 if let result = res {
                     var tricountName = String(describing: result)
                     let start = tricountName.index(tricountName.startIndex, offsetBy: 10)
@@ -57,7 +59,7 @@ func getInfoFromTricount(tricountID: String) async throws -> Tricount{
     let names: [String] = await withCheckedContinuation { continuation in
 
         Task{
-            await webViewStore.webView.evaluateJavaScript(getNames) { res, error in
+            await webView.evaluateJavaScript(getNames) { res, error in
                 if let result = res {
                     let namesString = String(describing: result)
                     //print(namesString)
