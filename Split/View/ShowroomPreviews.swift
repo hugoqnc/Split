73a119273804b5ff0bc5c2b9b9423a7847d1831d
Hugo Queinnec <67365803+hugoqnc@users.noreply.github.com>
2022-08-31 17:18:30 +0200

    add easy previews for demo screenshots

---
+++ Split/View/ShowroomPreviews.swift
@@ -0,0 +1,20 @@
+//
+//  ShowroomPreviews.swift
+//  Split
+//
+//  Created by Hugo Queinnec on 31/08/2022.
+//
+
+import SwiftUI
+
+struct ShowroomPreviews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ShowroomPreviews_Previews: PreviewProvider {
+    static var previews: some View {
+        ShowroomPreviews()
+    }
+}
